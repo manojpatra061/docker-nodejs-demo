c231e6eb69e2d78212a65a212e26a1d49e64d4c2
manojpatra061 <manojpatra061@gmail.com>
2025-09-06 15:51:56 +0530

    add things in part-1 branch

--- book-service/src/index.js
+++ book-service/src/index.js
@@ -1,17 +1,25 @@
 import express from "express";
-
-import Book from "./models/bookModel.js";
-import dbStart from "./db/db.js";
 import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-dotenv.config();
 const port = process.env.PORT || 3000;
 
-dbStart();
+const books = [
+  { id: 1, title: "1984", author: "George Orwell" },
+  {
+    id: 2,
+    title: "Brave New World",
+    author: "Aldous Huxley",
+  },
+  {
+    id: 3,
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+  },
+];
 
 app.get("/books", async (req, res) => {
-  const books = await Book.find({});
   res.json({ port, books });
 });
 
